15eea0175491b3404517a07ea3888eb5516bd249
Mihajatiana <miandriakotonerason@gmail.com>
2025-04-12 11:22:19 +0300

    add: setup supabase and react-hook-form

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://YOUR_PROJECT.supabase.co";
-const supabaseKey = "YOUR_PUBLIC_ANON_KEY";
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
